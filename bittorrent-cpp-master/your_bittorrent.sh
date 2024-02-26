b17e5555507b9948e0e36bf8dcbd41e10cae8b52
Kaushik Dwivedi <66638533+kaushikdutt97@users.noreply.github.com>
2024-02-26 14:29:58 +0530

    Update your_bittorrent.sh

--- bittorrent-cpp-master/your_bittorrent.sh
+++ bittorrent-cpp-master/your_bittorrent.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-#
-# DON'T EDIT THIS!
-#
-# CodeCrafters uses this file to test your code. Don't make any changes here!
-#
-# DON'T EDIT THIS!
+#file to test code. 
+
+
 set -e
 cmake $(dirname $0) -B build > /dev/null
 cmake --build build > /dev/null
